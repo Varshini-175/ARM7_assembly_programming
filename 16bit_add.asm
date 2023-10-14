b1901f42650aecf07931442ec082a46d76dbf37a
varshini-175 <varshinistudy@gmail.com>
2023-10-14 23:26:46 +0530

    half word addition

--- 16bit_add.asm
+++ 16bit_add.asm
@@ -5,7 +5,7 @@ RESULT EQU  R0
 	MOV R2, #0            ; Initialize the sum in R2 to 0
     MOV R4, #5            ; Initialize R4 with the array length
 SUM_LOOP
-	LDR R3, [R1], #4      ; Load the next element into R3
+	LDRH R3, [R1], #2      ; Load the next element into R3
     ADD R2, R2, R3        ; Add R3 to the sum in R2
     SUBS R4, R4, #1       ; Decrement the loop counter
     BNE SUM_LOOP          ; Branch if not equal (loop until R4 is 0)
@@ -13,5 +13,5 @@ SUM_LOOP
 HERE B HERE
 
 	AREA ARRAY_DATA, DATA,READONLY
-	DCW 5, 10, 15, 20, 25
+	DCW 1, 2, 3, 4, 5
 	END
